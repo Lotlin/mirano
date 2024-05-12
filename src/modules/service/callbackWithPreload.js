3404930a1118254f6b0ca50c3b68487794aa9cea
Lotlin <lotlin22@gmail.com>
2024-05-12 18:46:22 +0200

    fixed

--- src/modules/service/callbackWithPreload.js
+++ src/modules/service/callbackWithPreload.js
@@ -2,13 +2,13 @@ import {renderPreload} from '@/modules/render/renderPreload';
 import {removeElem} from '@/modules/util';
 
 export const callbackWithPreload = async (elem, cb, ...params) => {
-  const preload = renderPreload(elem, 'show');
+  const preload = renderPreload(elem);
 
   try {
     const result = await cb(...params);
 
     return result;
   } finally {
-    removeElem(preload);
+    removeElem(preload, elem);
   }
 };
