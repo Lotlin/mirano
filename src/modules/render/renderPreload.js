af7cf551ea55042c6012e78fb828409f1d7612c0
Lotlin <lotlin22@gmail.com>
2024-05-12 15:32:23 +0200

    upgrade

--- src/modules/render/renderPreload.js
+++ src/modules/render/renderPreload.js
@@ -1,22 +1,3 @@
-/* export const renderPreload = async (elem, cb, ...params) => {
-  const preload = document.createElement('div');
-
-  preload.classList = 'preload';
-
-  elem.style.position = 'relative';
-  elem.append(preload);
-  preload.style.display = 'flex';
-
-  try {
-    const result = await cb(...params);
-
-    return result;
-  } finally {
-    preload.style.display = 'none';
-    preload.remove();
-  }
-};
-*/
 export const renderPreload = (elem) => {
   const preload = document.createElement('div');
   preload.classList = 'preload';
