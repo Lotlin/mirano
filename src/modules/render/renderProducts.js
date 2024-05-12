19a64eb77e46aa8dba8376d0b99e9be167bee91f
Lotlin <lotlin22@gmail.com>
2024-05-12 12:08:38 +0200

    searchProducts

--- src/modules/render/renderProducts.js
+++ src/modules/render/renderProducts.js
@@ -8,6 +8,16 @@ export const renderProducts = async () => {
 
     goodsList.innerHTML = '';
 
+    if (!products.length) {
+      const messageItem = document.createElement('li');
+      messageItem.className = 'goods__no-product';
+      messageItem.textContent = 'Товары не найдены';
+
+      goodsList.append(messageItem);
+
+      return;
+    }
+
     products.forEach(product => {
       const productCard = ProductCard(product);
 
